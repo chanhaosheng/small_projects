f35e5f7a00eb9b679f0f7270112895dacc92f01f
Your Name <chanhaosheng@gmail.com>
2024-05-28 09:18:09 +0800

    shift handsplorers to small project

---
+++ 01_handsplorers/src/config.py
@@ -0,0 +1,19 @@
+# Configuration for recheck, mouse, and stay_on_top
+recheck = True
+mouse = False
+stay_on_top = True
+distance_x = 20
+distance_y = 20
+
+# Configuration for keystrokes
+keystrokes = {
+    "okay": "",
+    "peace": "",
+    "thumbs_down": "pagedown",
+    "thumbs_up": "pageup",
+    "call_me": "",
+    "live long": "",
+    "fist": "",
+    "smile": "",
+    "sleep": 3
+}
